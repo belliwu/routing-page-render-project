720463c0bb6f2f765824880b9303e8ab978399e4
belliwu <belliwu56@gmail.com>
2025-12-05 21:51:41 +0800

    chore: initialize backend package with dependencies and scripts

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,11 +1,18 @@
 import NewsList from "@/components/news-list";
-import { DUMMY_NEWS } from "@/dummy-news";
 
-export default function NewsPage() {
+export default async function NewsPage() {
+  const response = await fetch("http://127.0.0.1:8080/news");
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch news");
+  }
+
+  const news = await response.json();
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {<NewsList news={news} />}
     </>
   );
 }
