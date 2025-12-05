fe76aa866ab477624ef9297c7f487736abbb1073
belliwu <belliwu56@gmail.com>
2025-12-05 18:13:49 +0800

    重構新聞詳情頁面，將圖片連結包裝在 Link 元件中，並新增圖片頁面以顯示完整圖片

--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -1,9 +1,11 @@
-import { DUMMY_NEWS } from "@/dummy-news";
 import { notFound } from "next/navigation";
+import Link from "next/link";
+
+import { DUMMY_NEWS } from "@/dummy-news";
 
 export default function NewsDetailPage({ params }) {
-  const newSlug = params.slug;
-  const newsItem = DUMMY_NEWS.find((item) => item.slug === newSlug);
+  const newsSlug = params.slug;
+  const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug);
 
   if (!newsItem) {
     notFound();
@@ -12,12 +14,13 @@ export default function NewsDetailPage({ params }) {
   return (
     <article className="news-article">
       <header>
-        <img src={`/images/news/${newsItem?.image}`} alt={newsItem?.title} />
-        <h1>{newsItem?.title}</h1>
-        <time dateTime={newsItem?.date}>{newsItem?.date}</time>
+        <Link href={`/news/${newsItem.slug}/image`}>
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        </Link>
+        <h1>{newsItem.title}</h1>
+        <time dateTime={newsItem.date}>{newsItem.date}</time>
       </header>
-
-      <p>{newsItem?.content}</p>
+      <p>{newsItem.content}</p>
     </article>
   );
 }
